62e485fd5c84bebb981022a85202670810eccda8
Brandon Weng <wengbrandon@gmail.com>
2017-01-23 03:10:49 -0500

    heroku

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
+'use strict'
+console.log("Messenger Bot is starting...");
+
 const express = require("express");
 const Botly = require("botly");
 const botly = new Botly({
     accessToken: 'EAAaGpOO0SCEBAAgAXW4rkGtfXWT8E4d1z9oPyJ6xRnpbJJxWmhc3ETXjiwRFiQgPKsaeyZC9KVOJfTcHNSchErByyfVKfDvZAZAXmQP4b4LUhV3Pm6K4lF2TIKjzTfmTaJ0Ahr0QLm11aoHQgwh23GfaPc1k0Da7EQRKRKTYwZDZD', //page access token provided by facebook
-    verifyToken: 'i_am_brandon',
+    verifyToken: 'my_voice_is_my_password_verify_me',
     webHookPath: 'https://messengerbotwavinc.herokuapp.com/webhook/'
-    notificationType: Botly.CONST.REGULAR //already the default (optional),
 });
 
 botly.on("message", (senderId, message, data) => {
     let text = `echo: ${data.text}`;
-
     botly.sendText({
       id: senderId,
       text: text
