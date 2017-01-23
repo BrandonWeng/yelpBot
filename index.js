718cae10894169fd71fd546e67e62ba6f4355bd3
Brandon Weng <wengbrandon@gmail.com>
2017-01-23 03:01:03 -0500

    added botly modules for messenger development

--- index.js
+++ index.js
@@ -1,31 +1,21 @@
-console.log("Yelp,I'm Hungry is starting ...");
-
-// var token = 'EAAaGpOO0SCEBANtXJrv2eBDSJqtrHajmUwPnwY50OQkU2SRlOWqJZBU66cd8ZBPyjgE5jmg1odA7ueHh4iWkuBcnj8njXAUkLGAK3oUgxliPjMgAZCii60Mp8idpUlYoREx3dG5Cw66HA70bLo2htbOLck1DRav1Gp6L4zWigZDZD';
-
-var Yelp = require('yelp-api-v3');
-var yelp = new Yelp(require('./config'));
-
-function yelpTheFollowing(term,location,price,makeCards){
-
-  var yelpSearch = {
-    term: term,
-    category_filter:'food' ,
-    location: location,
-    limit:'5',
-    sort:'2',
-    is_closed:'false',
-    price: price,
-    open_now:'true'
-  };
-
-  yelp.search(yelpSearch,printYelp);
-
-  function printYelp(err,data){
-    if (err) return console.error("Yelp, Something went wrong! :" + err);
-    makeCards(JSON.parse(data));
-  }
-}
-
-yelpTheFollowing('chinese','Toronto','2',function(data){
-  console.log(data);
+const express = require("express");
+const Botly = require("botly");
+const botly = new Botly({
+    accessToken: 'EAAaGpOO0SCEBAAgAXW4rkGtfXWT8E4d1z9oPyJ6xRnpbJJxWmhc3ETXjiwRFiQgPKsaeyZC9KVOJfTcHNSchErByyfVKfDvZAZAXmQP4b4LUhV3Pm6K4lF2TIKjzTfmTaJ0Ahr0QLm11aoHQgwh23GfaPc1k0Da7EQRKRKTYwZDZD', //page access token provided by facebook
+    verifyToken: 'i_am_brandon',
+    webHookPath: 'https://messengerbotwavinc.herokuapp.com/webhook/'
+    notificationType: Botly.CONST.REGULAR //already the default (optional),
 });
+
+botly.on("message", (senderId, message, data) => {
+    let text = `echo: ${data.text}`;
+
+    botly.sendText({
+      id: senderId,
+      text: text
+    });
+});
+
+const app = express();
+app.use("/webhook", botly.router());
+app.listen(3000);
