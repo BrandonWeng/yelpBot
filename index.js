a1710325b42925d86da219d5245bbd519315be91
Brandon Weng <wengbrandon@gmail.com>
2017-01-24 02:42:13 -0500

    testing buttons

--- index.js
+++ index.js
@@ -29,19 +29,24 @@ app.get('/webhook/', function (req, res) {
 
 // Spin up the server
 app.listen(app.get('port'), function() {
-    console.log('running on port', app.get('port'))
+    console.log('Successfully Started')
 })
 
+app.post('/webhook/', messageRecieved);
 
-app.post('/webhook/', function (req, res) {
+function messageRecieved(req, res) {
     let messaging_events = req.body.entry[0].messaging
     for (let i = 0; i < messaging_events.length; i++) {
         let event = req.body.entry[0].messaging[i]
         let sender = event.sender.id
         if (event.message && event.message.text) {
             let text = event.message.text
-            if (text === 'Generic') {
-                sendGenericMessage(sender)
+            if (text === 'Start') {
+                sendStartMessage(sender)
+                continue
+            }
+            if (text === 'Price'){
+                sendPriceRangeButton(sender)
                 continue
             }
             sendTextMessage(sender, "Testing Postbacks!")
@@ -53,7 +58,7 @@ app.post('/webhook/', function (req, res) {
         }
     }
     res.sendStatus(200)
-})
+}
 
 function sendTextMessage(sender, text) {
     let messageData = { text:text }
@@ -74,44 +79,75 @@ function sendTextMessage(sender, text) {
     })
 }
 
-function sendGenericMessage(sender) {
+function sendStartMessage(sender) {
     let messageData = {
         "attachment": {
             "type": "template",
             "payload": {
-                "template_type": "generic",
-                "elements": [{
-                    "title": "First card",
-                    "subtitle": "Element #1 of an hscroll",
-                    "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
-                    "buttons": [{
-                        "type": "web_url",
-                        "url": "https://www.messenger.com",
-                        "title": "web url"
-                    }, {
-                        "type": "postback",
-                        "title": "Postback",
-                        "payload": "Payload for first element in a generic bubble",
-                    }],
-                }, {
-                    "title": "Second card",
-                    "subtitle": "Element #2 of an hscroll",
-                    "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-                    "buttons": [{
-                        "type": "postback",
-                        "title": "Postback",
-                        "payload": "Payload for second element in a generic bubble",
-                    }],
-                },{
-                    "title": "Third card",
-                    "subtitle": "Element #3 of an hscroll",
-                    "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-                    "buttons": [{
-                        "type": "postback",
-                        "title": "Postback",
-                        "payload": "Payload for second element in a generic bubble",
-                    }]
-                }]
+                "template_type": "button",
+                "text":"Hey! are you hungry?",
+                "buttons":[
+                    {
+                        "type":"postback",
+                        "title":"Yes!",
+                        "payload":"hungry"
+                    },
+                    {
+                        "type":"web_url",
+                        "title":"Nope",
+                        "payload":"notHungry"
+                    }
+                    ]
+            }
+        }
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+json: {
+    recipient: {id:sender},
+    message: messageData,
+}
+}, function(error, response, body) {
+    if (error) {
+        console.log('Error sending messages: ', error)
+    } else if (response.body.error) {
+        console.log('Error: ', response.body.error)
+    }
+})
+}
+
+
+function sendPriceRangeButton(sender) {
+    let messageData = {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "button",
+                "text":"Cool! What's the budget?",
+                "buttons":[
+                    {
+                        "type":"postback",
+                        "title":"Cheap, but not too cheap",
+                        "payload":"1"
+                    },
+                    {
+                        "type":"postback",
+                        "title":"Give me the average",
+                        "payload":"2"
+                    },
+                    {
+                        "type":"postback",
+                        "title":"Treat myself a bit tonight",
+                        "payload":"3"
+                    },
+                    {
+                        "type":"postback",
+                        "title":"It's payday!",
+                        "payload":"4"
+                    }
+                ]
             }
         }
     }
@@ -131,3 +167,4 @@ function sendGenericMessage(sender) {
         }
     })
 }
+
