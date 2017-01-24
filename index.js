a1257ef93de8402173d1db8e4dce3e329438d463
Brandon Weng <wengbrandon@gmail.com>
2017-01-24 01:30:31 -0500

    test heroku

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json())
 
 // Index route
 app.get('/', function (req, res) {
-    res.send('Hello world, I am a chat bot')
+    res.send('Facebook Tokens Verified!')
 })
 
 // for Facebook verification
