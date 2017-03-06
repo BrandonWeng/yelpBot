933940afbf8fb6a1130b349a86ec61f35a40cf16
Brandon Weng <wengbrandon@gmail.com>
2017-03-05 22:05:30 -0500

    added option to repick price

--- fbconfig.js
+++ fbconfig.js
@@ -6,8 +6,8 @@
 
 // Facebook APP tokens
 // Facebook secret verify tokens
-let token = 'EAAaGpOO0SCEBACKheumfl9Pz72WD4ZAE1deRl0OQZB7BnFNlegbSLJJIDE9IfJRTLjZCcLAke88GdChPyzfjHtK5cgaJkj9qAYIcsaoSZCt108wDD5N7W6oQg2klxXfV2rYHSkcIRhbZAH9CT66ZB22QyoEuHZAguE2fvtsEvhtUwZDZD';
-let verify_token = 'yelp_im_hungry2017';
+let token = 'KEY';
+let verify_token = 'KEY';
 
 // Constant Text messages to send to users
 let sentRestaurant = "Here are some of the top picks! These resturants should be open :). I hope it's what you were looking for!";
